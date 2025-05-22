52bd178eb968c81de4b04da17b245fc68de057cd
Fives2 <gamersbr.bolo@gmail.com>
2025-05-22 18:32:48 -0300

    Created main class

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-read me.
+# Databank
+
+```
+CREATE TABLE category (
+    catcode SERIAL PRIMARY KEY,                  
+    catdescription VARCHAR(100) NOT NULL,        
+    catactive BOOLEAN NOT NULL DEFAULT TRUE      
+);
+```
---
+++ includes/commonFiles/classes/class_crud_main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Includes\CommonFiles\Classes\CrudMain;
+/**
+ * Base class of the crud
+ */
+class CrudMain {
+    
+    /**
+     * method that inserts data in the bank
+     * 
+     * @return void
+     */
+    protected function insertData(){
+
+
+    }
+
+    // protected function alterData(){
+
+    // }
+
+    // protected function deleteData()    {
+
+    // }
+}
